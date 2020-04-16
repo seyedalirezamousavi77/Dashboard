1b8952fa4c1d16b3762039578836ecc2ddc170f4
seyed alireza mousavi <s.alireza.m8888@gmail.com>
2020-04-16 06:45:36 +0430

    create a main.js for fixed navbar menu by scrolling

--- js/main.js
+++ js/main.js
@@ -1,24 +1,14 @@
-jQuery(document).ready(function($){
-	$('.web-counter-number').counterUp({
-		
-	});
-	var menu = $('.web-header-section');
-	$(window).scroll(function(){
-		if($(this).scrollTop() > 0 && menu.hasClass('web-header-section')){
-			menu.addClass('web-header-fixed');
-		}
-		else if($(this).scrollTop() <= 0 && menu.hasClass('web-header-fixed')){
-			menu.removeClass('web-header-fixed');
-		}
-	});
-});
-$(document).on("click", '[data-toggle="lightbox"]', function(event) {
-      event.preventDefault();
-      $(this).ekkoLightbox();
-    });
 
-$(document).on("click", 'ul li', function() {
-      $(this).addClass('active').siblings().removeClass('active');
-    });
+function navbarScroll() {
+	var navbar = document.getElementById("my-navbar");
+	var sticky = navbar.offsetTop + 10;
+	if (window.pageYOffset > sticky) {
+		navbar.classList.add("sticky");
+		navbar.style.display = "flex";
+	} else {
+		navbar.classList.remove("sticky");
+		navbar.style.display = "none";
+	}
+}
 
 
